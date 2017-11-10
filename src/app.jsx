950234564518705a6815758c5f83109f328d41fa
Berend Weij <info@berendweij.nl>
2017-11-10 10:44:26 +0100

    removed md2 code

--- src/app.jsx
+++ src/app.jsx
@@ -3,31 +3,7 @@ import Student from './components/student';
 
 class App extends React.Component {
 
-    constructor(props){
-        super(props);
-
-        this.students = [
-            {name:'Bob', age: 17},
-            {name:'Bob2', age: 18},
-            {name:'Bob3', age: 19},
-            {name:'Bob4', age: 20},
-            {name:'Bob5', age: 21},
-            {name:'Bob6', age: 31}
-        ];
-    }
-
-    showStudent(currentStudent, i){
-        return (
-            <Student
-                key={i}
-                name={currentStudent.name}
-                age={currentStudent.age}
-            />
-        );
-    }
-
     render() {
-
         return (
             <div className="app">
                 MD2 React app om te oefenen
@@ -37,12 +13,8 @@ class App extends React.Component {
                 />
                 <Student />
                 <Student />
-                <Student
-                    name="Bob"
-                />
                 <Student />
-                Ik wil nog wat extra's laten zien:
-                {this.students.map(this.showStudent)}
+                <Student />
             </div>
         );
     }
